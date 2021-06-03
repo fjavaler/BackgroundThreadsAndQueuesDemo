6ead37e240ddf847f8f926030d8aeb4ecc5afcc0
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 17:58:58 -0600

    Initial Commit

---
+++ BackgroundThreadsAndQueuesDemo/BackgroundThreadsAndQueuesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundThreadsAndQueuesDemoApp.swift
+//  BackgroundThreadsAndQueuesDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundThreadsAndQueuesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
